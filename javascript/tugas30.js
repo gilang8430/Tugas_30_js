d114690dced1de9ad4e9a580464a914396abf2a0
gilang_gans <gilangsos99@gmail.com>
2022-05-21 22:32:34 +0700

    Tugas 30 js
    Tugas ini untuk niomic

---
+++ javascript/tugas30.js
@@ -0,0 +1,8 @@
+function Regex(){
+  var str = ("para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
+  var data = str.match(/[e,s]/g);
+
+  console.log(data);
+}
+
+Regex();
